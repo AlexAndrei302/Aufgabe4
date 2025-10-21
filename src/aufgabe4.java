0d92d7648963d1c364753972be2a45230a5b5b72
AlexAndrei302 <andreialexpaul10@gmail.com>
2025-10-21 17:05:04 +0300

    final commit

--- src/aufgabe4.java
+++ src/aufgabe4.java
@@ -1,3 +1,4 @@
+
 public class aufgabe4 {
 
     // 1) findet die billigste Tastatur
@@ -10,43 +11,55 @@ public class aufgabe4 {
         }
         return min;
     }
-}
-// 2) findet den teuersten Gegenstand (Tastatur oder USB)
-public int mostExpensive(int[] keyboards, int[] usbs) {
-    int max = keyboards[0];
 
-    for (int k : keyboards)
-        if (k > max) max = k;
+    // 2) findet den teuersten Gegenstand (Tastatur oder USB)
+    public int mostExpensive(int[] keyboards, int[] usbs) {
+        int max = keyboards[0];
 
-    for (int u : usbs)
-        if (u > max) max = u;
+        for (int k : keyboards)
+            if (k > max) max = k;
 
-    return max;
-}
+        for (int u : usbs)
+            if (u > max) max = u;
 
-void main() {
-}
-// 3) findet das teuerste USB, das Markus sich leisten kann
-public int bestUsbWithinBudget(int[] usbs, int budget) {
-    int best = -1;
-    for (int u : usbs) {
-        if (u <= budget && u > best) {
-            best = u;
-        }
+        return max;
     }
-    return best;
-}
 
-// 4) findet maximale Summe (Tastatur + USB), die ins Budget passt
-public int maxSpend(int budget, int[] keyboards, int[] usbs) {
-    int max = -1;
-    for (int k : keyboards) {
+    // 3) findet das teuerste USB, das Markus sich leisten kann
+    public int bestUsbWithinBudget(int[] usbs, int budget) {
+        int best = -1;
         for (int u : usbs) {
-            int sum = k + u;
-            if (sum <= budget && sum > max) {
-                max = sum;
+            if (u <= budget && u > best) {
+                best = u;
             }
         }
+        return best;
     }
-    return max;
+
+    // 4) findet maximale Summe (Tastatur + USB), die ins Budget passt
+    public int maxSpend(int budget, int[] keyboards, int[] usbs) {
+        int max = -1;
+        for (int k : keyboards) {
+            for (int u : usbs) {
+                int sum = k + u;
+                if (sum <= budget && sum > max) {
+                    max = sum;
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        aufgabe4 a = new aufgabe4();
+
+        int[] keyboards = {50, 20, 75};
+        int[] usbs = {5, 10, 15};
+        int budget = 60;
+
+        System.out.println("Cheapest keyboard: " + a.cheapestKeyboard(keyboards));
+        System.out.println("Most expensive item: " + a.mostExpensive(keyboards, usbs));
+        System.out.println("Best USB within budget (" + budget + "): " + a.bestUsbWithinBudget(usbs, budget));
+        System.out.println("Max spend within budget (" + budget + "): " + a.maxSpend(budget, keyboards, usbs));
+    }
 }
